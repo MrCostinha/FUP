5ddf0a188eb268462b8e35e48a62b2a1fd2748f1
Matheus Costa <matheuscostamc135@gmail.com>
2023-03-31 20:52:05 -0300

    Correção lógica Q7 e Q8

--- Lista A-6.c
+++ Lista A-6.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int dia_nascimento, mes_nascimento, ano_nascimento, qtd_dias_nascimento, qtd_dias_atual = 728400, idade;
+    int dia_nascimento, mes_nascimento, ano_nascimento, qtd_dias_nascimento, qtd_dias_atual, idade;
 
     printf("Insira data de nascimento (dd/mm/aa): \n");
     scanf("%d%d%d", &dia_nascimento, &mes_nascimento, &ano_nascimento);
 
+    qtd_dias_atual = 2023*360 + 3*30 + 31;
     qtd_dias_nascimento = ano_nascimento*360 + mes_nascimento*30 + dia_nascimento;
     idade = qtd_dias_atual - qtd_dias_nascimento;
+    
     idade = idade/360;
 
     printf("Idade em anos = %d", idade);
--- Lista A-8.c
+++ Lista A-8.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int dia_nascimento, mes_nascimento, ano_nascimento, qtd_dias_nascimento, qtd_dias_atual, idade_anos, idade_meses, idade_dias;
+    int dia_nascimento, mes_nascimento, ano_nascimento, qtd_dias_nascimento, dia_atual, mes_atual, ano_atual, qtd_dias_atual, idade_anos, idade_meses, idade_dias;
 
+    printf("Insira a data atual (dd/mm/aa): \n");
+    scanf("%d%d%d", &dia_atual, &mes_atual, &ano_atual);
     printf("Insira data de nascimento (dd/mm/aa): \n");
     scanf("%d%d%d", &dia_nascimento, &mes_nascimento, &ano_nascimento);
 
-    qtd_dias_atual = 2023*360 + 3*30 + 31;
+    qtd_dias_atual = ano_atual*360 + mes_atual*30 + dia_atual;
     qtd_dias_nascimento = ano_nascimento*360 + mes_nascimento*30 + dia_nascimento;
     idade_dias = qtd_dias_atual - qtd_dias_nascimento;
-
+    
     idade_anos = idade_dias/360;
     idade_meses = (idade_dias%360)/30;
     idade_dias = (idade_dias%360)%30;
