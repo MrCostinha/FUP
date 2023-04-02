190293922ffb25ed5c51cdcbad541c85f624c98b
Matheus Costa <matheuscostamc135@gmail.com>
2023-04-01 23:07:48 -0300

    Questão 13

---
+++ Lista A-13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int angulo;
+
+    printf("Informe o ângulo em graus: ");
+    scanf("%d", &angulo);
+
+    if (angulo>360) {
+        angulo %= 360;
+    }
+    else if (angulo<0) {
+        angulo %= 360;
+        angulo += 360;
+    }
+    
+    if (angulo>=0 && angulo<=90) {
+        printf("Quadrante: 1");
+    }
+    else if (angulo>90 && angulo<=180) {
+        printf("Quadrante: 2");
+    }
+    else if (angulo>180 && angulo<=270) {
+        printf("Quadrante: 3");
+    }
+    else {
+        printf("Quadrante: 4");
+    }
+    
+    return 0;
+}
