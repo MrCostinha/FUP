3e3f58854120e6aed98d3c6b159d1d258074e0d1
Matheus Costa <matheuscostamc135@gmail.com>
2023-04-13 20:51:30 -0300

    Questões 5, 6

--- Arnaldo/Lista de Fixação/Parte 2 - 05.c
+++ Arnaldo/Lista de Fixação/Parte 2 - 05.c
@@ -5,20 +5,19 @@
 int main() {
     setlocale(LC_ALL, "");
 
-    float numero, i = 1.0, soma = 0;
+    float numero, i = 0, soma = 0;
 
     wprintf(L"Números reais: \n");
     
-    while (1) {
+    while (numero!=0) {
         scanf("%f", &numero);
-        if (numero==0) {
-            break;
+        if (numero!=0){
+            soma += numero;
+            i++;
         }
-        soma += numero;
-        i++;
     }
 
-    wprintf(L"Média dos números reais: %.2f", soma/(i-1));
+    wprintf(L"Média dos números reais: %.2f", soma/i);
 
     return 0;
 }
---
+++ Arnaldo/Lista de Fixação/Parte 2 - 06.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int x, maior, menor, count = 0;
+
+    wprintf(L"Números inteiros: \n");
+    
+    do {
+        count++;
+        scanf("%d", &x);
+        if (count==1) {
+            maior = x;
+            menor = x;
+        }
+        if (x!=0) {
+            if (x>=maior) {
+                maior = x;
+            }
+            else if (x<=menor) {
+                menor = x;
+            }
+        }
+    } while (x!=0);
+
+    wprintf(L"Maior: %d\nMenor: %d", maior, menor);
+
+    return 0;
+}
