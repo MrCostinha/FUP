5102281d2aa0dcc076306bf957db0c1af7855cd0
Matheus Costa <matheuscostamc135@gmail.com>
2023-05-04 18:02:16 -0300

    PT2 Q15

---
+++ Arnaldo/Lista de Fixação/Parte 2 - 15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int numero_1, numero_2, mdc, mmc;
+
+    wprintf(L"Digite dois números inteiros: ");
+    scanf("%d%d", &numero_1, &numero_2);
+
+    for (int i = 1; i <= numero_1 && i <= numero_2; i++) {
+        if (numero_1 % i == 0 && numero_2 % i == 0) {
+            mdc = i;
+        }
+    }
+
+    mmc = (numero_1*numero_2)/mdc;
+
+    printf("MMC(%d, %d) = %d", numero_1, numero_2, mmc);
+
+    return 0;
+}
