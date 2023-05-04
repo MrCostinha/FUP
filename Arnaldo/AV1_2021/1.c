c22071c89f902f60f4d08379f08ae9afd41c8871
Matheus Costa <matheuscostamc135@gmail.com>
2023-05-04 20:14:37 -0300

    1 Correta

--- Arnaldo/AV1_2021/1.c
+++ Arnaldo/AV1_2021/1.c
@@ -1,3 +1,8 @@
+/*O código abaixo visa fazer um jogo de dados onde os n jogadores rolam 2 dados de 6 lados e ganha
+aquele que tirou a maior soma (em caso de empate, o que jogou primeiro ganha). Ao final do jogo era
+para o número do jogador (de 1 até n) ser impresso, mas em alguma parte o jogo não funciona como
+deveria. Identifique o que está errado e como podemos consertar o código.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -5,8 +10,8 @@
 int main() {
     srand(time(NULL));
     int n, p=1, m=0, v;
-    scanf("%d ", n);
-    while (p>=n) {
+    scanf("%d", &n);
+    while (p<=n) {
         int d1, d2;
         d1 = rand()%6+1;
         d2 = rand()%6+1;
@@ -15,6 +20,7 @@ int main() {
             v = p;
             m = d1+d2;
         }
+        p++;
     }
     printf("O vencedor foi o jogador %d.", v) ;
     return 0;
