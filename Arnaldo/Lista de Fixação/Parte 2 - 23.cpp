30f238d731c7a172afe5286067624efa0f89a1a0
Matheus Costa <matheuscostamc135@gmail.com>
2023-05-15 15:31:12 -0300

    Desisti dessa desgraça por enquanto

--- Arnaldo/Lista de Fixação/Parte 2 - 23.cpp
+++ Arnaldo/Lista de Fixação/Parte 2 - 23.cpp
@@ -10,52 +10,59 @@ int main() {
     setlocale(LC_ALL, "");
     srand(time(NULL));
 
-    int jogadores, maior = 0, qtd_empatados = 0;
+    int jogadores, maior = 0, empatados = 0;
     bool empate = false;
 
     cout << "Quantidade de jogadores: ";
     cin >> jogadores;
     cout << endl;
-    int jogador[jogadores], empatados[jogadores];
+
+    int jogador[jogadores], empatado[jogadores];
 
     for (int i = 0; i < jogadores; i++) {
         jogador[i] = rand()%6+1;
         cout << "Jogador " << i+1 << ": " << jogador[i] << endl;
 
-        if (i == 0) {
-            maior = i;
-        }
         if (jogador[i] > jogador[maior]) {
             maior = i;
         } else if (jogador[i] == jogador[maior]) {
-            empatados[i] = jogador[i];
-            qtd_empatados ++;
+            empatado[empatados] = jogador[i];
+            empatados ++;
             empate = true;
         }
     }
-    
-    while (empate == true) {
+
+    while (empate) {
         cout << "\nEmpatados: ";
-        
-        for (int i = 0; i < qtd_empatados; i++) {
-            if (jogador[i] == empatados[i]) {
-                cout << empatados[i] << " ";
-                
-            }
+
+        for (int i = 0; i < empatados; i++) {
+            cout << "Jogador " << empatado[i] << " ";
         }
 
         cout << endl << "\nNovo sorteio\n" << endl;
 
-        for (int i = 0; i < qtd_empatados; i++) {
-            empatados[i] = rand()%6+1;
-            cout << "Jogador " << i+1 << ": " << empatados[i] << endl;
+        for (int i = 0; i < empatados; i++) {
+            jogador[empatado[i]] = rand()%6+1;
+            cout << "Jogador " << i+1 << ": " << jogador[empatado[i]] << endl;
 
-            if (empatados[i] > empatados[maior]) {
-                maior = i;
+            if (jogador[empatado[i]] > jogador[maior]) {
+                maior = empatado[i];
                 empate = false;
-                break;
+            } else if (jogador[empatado[i]] == jogador[maior]) {
+                empate = true;
             }
-            empate = false;
+        }
+
+        if (!empate) {
+            break;
+        }
+
+        empatados = 0;
+        for (int i = 0; i < jogadores; i++) {
+            if (jogador[i] == jogador[maior]) {
+                empatado[empatados] = i;
+                empatados ++;
+            }
         }
     }
 
