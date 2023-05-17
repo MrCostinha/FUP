a9fad572213133e64bf4403bff864ac67c90d53b
Matheus Costa <matheuscostamc135@gmail.com>
2023-05-17 07:24:31 -0300

    Lógica do krl q o Raylander me mostrou

---
+++ Arnaldo/Lista de Fixação/Parte 2 - 29.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <wchar.h>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int n, meio, aux, count = 0;
+
+    wcout << L"Digite um número: ";
+    cin >> n;
+    meio = (n/2) + 1;
+    aux = meio;
+
+    for (int linha = 1; linha <= n; linha++) {
+        for (int coluna = 1; coluna <= n; coluna++) {
+            if (coluna == meio) {
+                cout << "* ";
+            }
+            if (coluna < meio) {
+                if (coluna >= aux) {
+                    cout << "* ";
+                    count++;
+                } else {
+                    cout << "  ";
+                }
+            }
+            if (coluna >= meio) {
+                if (count > 0) {
+                    cout << "* ";
+                    count--;
+                }
+            }
+        }
+        if (linha < meio) {
+            aux--;
+        } else {
+            aux++;
+        }
+        
+        cout << endl;
+        count = 0;
+    }
+
+    return 0;
+}
