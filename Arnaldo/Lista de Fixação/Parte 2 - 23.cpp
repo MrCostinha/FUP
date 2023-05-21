540326a2c767a605d966761b82b4e7328040b555
Matheus Costa <matheuscostamc135@gmail.com>
2023-05-20 23:50:07 -0300

    Att lógica

--- Arnaldo/Lista de Fixação/Parte 2 - 23.cpp
+++ Arnaldo/Lista de Fixação/Parte 2 - 23.cpp
@@ -10,7 +10,7 @@ int main() {
     setlocale(LC_ALL, "");
     srand(time(NULL));
 
-    int jogadores, maior = 0, empatados = 0;
+    int jogadores, maior = 0, empatados = 1;
     bool empate = false;
 
     cout << "Quantidade de jogadores: ";
@@ -21,52 +21,32 @@ int main() {
 
     for (int i = 0; i < jogadores; i++) {
         jogador[i] = rand()%6+1;
-        cout << "Jogador " << i+1 << ": " << jogador[i] << endl;
 
+        cout << "Jogador " << i+1 << ": " << jogador[i] << endl;
+        
         if (jogador[i] > jogador[maior]) {
             maior = i;
-        } else if (jogador[i] == jogador[maior]) {
-            empatado[empatados] = jogador[i];
+            empatado[0] = i+1;
+            empate = false;
+        } else if (jogador[i] == jogador[maior] && i != maior) {
+            empatado[empatados] = i+1;
             empatados ++;
             empate = true;
         }
     }
 
-    while (empate) {
-        cout << "\nEmpatados: ";
-
+    if (empate) {
         for (int i = 0; i < empatados; i++) {
-            cout << "Jogador " << empatado[i] << " ";
+            cout <<"\nEmpatado " << i+1 << ": Jogador " << empatado[i] << " ";
         }
-
-        cout << endl << "\nNovo sorteio\n" << endl;
-
-        for (int i = 0; i < empatados; i++) {
-            jogador[empatado[i]] = rand()%6+1;
-            cout << "Jogador " << i+1 << ": " << jogador[empatado[i]] << endl;
-
-            if (jogador[empatado[i]] > jogador[maior]) {
-                maior = empatado[i];
-                empate = false;
-            } else if (jogador[empatado[i]] == jogador[maior]) {
-                empate = true;
-            }
-        }
-
-        if (!empate) {
-            break;
-        }
-
-        empatados = 0;
-        for (int i = 0; i < jogadores; i++) {
-            if (jogador[i] == jogador[maior]) {
-                empatado[empatados] = i;
-                empatados ++;
-            }
-        }
+        
+        maior = rand()%empatados+1;
+        
+        cout << "\nNovo sorteio entre os empatados" << endl;
+        wcout << "\nO empatado " << empatados << L" começa!";
+    } else {
+        wcout << "\nO jogador " << maior+1 << L" começa!";
     }
-
-    wcout << L"\nO jogador número " << maior+1 << L" começa";
 
     return 0;
 }
